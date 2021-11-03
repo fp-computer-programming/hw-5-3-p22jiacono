5b029cb080fa525d5edf72da5c555d519554e4d5
p22jiacono <90051927+p22jiacono@users.noreply.github.com>
2021-11-03 09:53:50 -0400

    add file hw5-3

---
+++ hw5-3.py
@@ -0,0 +1,15 @@
+#Author: JTI 11/2/21
+import calendar
+
+# Questrion 1
+calendar.TextCalendar().pryear(2020)
+
+# Question 2
+calendar.TextCalendar(6).pryear(2020)
+
+# Question 3
+calendar.prmonth(2021, 11)
+
+# Question 5
+print(calendar.isleap(2020))
+print(calendar.isleap(1995))
